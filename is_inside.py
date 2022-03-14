8434b9d61314551156e6536946650d3742f5fbab
Rousset Thibault <thibault.rousset@student-cs.fr>
2022-03-14 16:10:25 +0100

    is_inside implemented

--- is_inside.py
+++ is_inside.py
@@ -1,20 +1,15 @@
 import numpy as np
 
-def ray_tracing_method(x,y,poly):
+def intersect(point,x1,x2):
 
-    n = len(poly)
-    inside = False
+    return not(point[1] >= max(x1[1],x2[1]) or point[1] < min(x1[1],x2[1])) and point[0] < min(x1[0], x2[0])
 
-    p1x,p1y = poly[0]
-    for i in range(n+1):
-        p2x,p2y = poly[i % n]
-        if y > min(p1y,p2y):
-            if y <= max(p1y,p2y):
-                if x <= max(p1x,p2x):
-                    if p1y != p2y:
-                        xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
-                    if p1x == p2x or x <= xints:
-                        inside = not inside
-        p1x,p1y = p2x,p2y
-
-    return inside
+def is_inside(point, poly):
+    nb_intersect = 0
+    for i in range(0, len(poly)) :
+        if i==len(poly)-1 :
+            if intersect(point, poly[i], poly[0]):
+                nb_intersect+=1
+        elif intersect(point, poly[i], poly[i+1]):
+            nb_intersect+=1
+    return not(nb_intersect%2 == 0)
--- neighbors.py
+++ neighbors.py
@@ -1,7 +1,47 @@
-from SouffleBati2D import *
+#from SouffleBati2D import *
 from is_inside import *
+import numpy as np
+import matplotlib.pyplot as plt
 
-Bat_list = [Bat1,Bat2,Bat3,Bat4,Bat5,Bat6]
+Bat1 = [(10.08,10.223),
+        (30.08,10.223),
+        (30.08,15.223),
+        (17.08,15.223),
+        (17.08,25.223),
+        (10.08,25.223)]
+
+Bat2 = [(50.08,12.223),
+        (65.08,12.223),
+        (65.08,27.223),
+        (50.08,27.223)]
+
+Bat3 = [(80.08,32.223),
+        (87.08,32.223),
+        (87.08,57.223),
+        (80.08,57.223)]
+
+Bat4 = [(12.08,78.223),
+        (42.08,78.223),
+        (42.08,92.223),
+        (12.08,92.223)]
+
+Bat5 = [(75,70),
+        (85,80),
+        (90,75),
+        (96,81),
+        (0.5*167,0.5*187),
+        (0.5*135,0.5*155)]
+
+Bat6 = [(20.08,40.223),
+        (40.08,40.223),
+        (40.08,45.223),
+        (25.08,45.223),
+        (25.08,55.223),
+        (35.08,55.223),
+        (35.08,70.223),
+        (20.08,70.223)]
+
+Bat_list = [[tuple([5*x for x in i]) for i in Bat] for Bat in [Bat1,Bat2,Bat3,Bat4,Bat5,Bat6]]
   
 def get_pts_coord(pts_map,n,e):
     pts_coord = []
@@ -17,12 +57,11 @@ def make_graph(bat_coord, map_size=100.0, edge_size=10.0):
     pts_coord = get_pts_coord(pts_map, n=n_edge, e=edge_size)
     for bat in Bat_list:
         for e in pts_coord :
-            if ray_tracing_method(e[0], e[1], bat) :
-                pts_map[e[0], e[1]] = 0
-    return(pts_map)
+            if is_inside(e, bat) :
+                pts_map[int(e[0])//edge_size,int(e[1])//edge_size] = 0
+    return(np.rot90(pts_map, k=1, axes=(0, 1)))
 
-def get_neighbors(x0, y0, edge_size):
-    pts_map = make_graph(Bat_list, edge_size)
+def get_neighbors(pts_map, x0, y0):
 
     neighbors=[]
     num_neighbor = 1
@@ -42,3 +81,8 @@ def get_neighbors(x0, y0, edge_size):
         if pts_map(e) == 0:
             neighbors.remove(e)
     return neighbors
+
+G = make_graph(Bat_list, 500, 1)
+
+plt.imshow(G,interpolation='nearest', cmap = 'gray')
+plt.show()
