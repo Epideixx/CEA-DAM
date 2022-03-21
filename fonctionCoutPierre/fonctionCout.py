aa9e9ec556a9aecf3fc9e8f4c51a311966a42b12
Pierre Labouré <pierre.laboure@student-cs.fr>
2022-03-21 14:09:25 +0100

    ajout DTW

--- fonctionCoutPierre/fonctionCout.py
+++ fonctionCoutPierre/fonctionCout.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import csv
 import argparse
 from scipy import signal as sgn
+from tester import displayMatch
 
 deltaT = 9.03696114115064 * 1e-5
 nb_stations = 6
@@ -32,9 +33,10 @@ def getData(folder):
 
     for i in range(nb_stations):
         # axs[i].plot(data_t[i], data_sign[i])
-        plt.plot(data_t[i], data_sign[i], label='station' + str(i))
-        plt.legend()
-    plt.show()
+        a = 0
+        #plt.plot(data_t[i], data_sign[i], label='station' + str(i))
+        # plt.legend()
+    # plt.show()
 
     signalArray = np.empty((nb_stations, len(data_t[0]), 2))
 
@@ -82,17 +84,19 @@ def correlationSignals(signalArray1, signalArray2):
         autoCorr = sgn.correlate(np.abs(sig1k[:, 1]), np.abs(sig1k[:, 1]))
 
         axs[k, 0].plot(
-            lags, interCorr, label='correlation entre les signaux de la station' + str(k))
-        axs[k, 0].legend()
+            lags, interCorr, label='correlation entre les signaux de la station ' + str(k))
+        #axs[k, 0].legend()
 
         axs[k, 1].plot(sig1k[:, 0], sig1k[:, 1],
-                       label='signal 60 station' + str(k))
+                       label='signal original station ' + str(k))
         axs[k, 1].plot(sig2k[:, 0], sig2k[:, 1],
-                       label='signal 50 station' + str(k))
-        axs[k, 1].legend()
+                       label='signal autre explosion station ' + str(k))
+        #axs[k, 1].legend()
 
         interCorrArray[k, :] = interCorr
         autoCorrArray[k, :] = autoCorr
+    axs[0, 0].legend()
+    axs[0, 1].legend()
 
     plt.show()
     return interCorrArray, autoCorrArray, lags
@@ -131,6 +135,7 @@ def getDelay(signal1Stat1, signal2Stat1):
     sig2 = signal2Stat1[:, 1]
     firstMax1 = np.argmax(sig1 > 1000)
     firstMax2 = np.argmax(sig2 > 1000)
+
     delay = firstMax1 - firstMax2
     # le retard est calculé en terme d'indice de liste
     # On utilise un seuil d'une valeur de 1000 Pascal pour déterminer l'indice de début d'un signal
@@ -245,6 +250,7 @@ def createDelay(sigArray, delay):
 
 
 sigArray60Delayed = createDelay(signalArray60, -30)
+
 # a, b = removeDelay(signalArray60, sigArray60Delayed)
 
 
@@ -268,6 +274,65 @@ def cout(sigArray1, sigArray2):
     return dist
 
 
-dist = cout(signalArray60, signalArray40)
-dist = cout(signalArray60, signalArray55)
-dist = cout(signalArray60, sigArray60Delayed)
+#dist = cout(signalArray60, signalArray40)
+#dist = cout(signalArray60, signalArray55)
+#dist = cout(signalArray60, sigArray60Delayed)
+
+
+def normalize(signal):
+    norm = np.linalg.norm(signal[:, 1])
+    signal[:, 1] /= norm
+
+
+def coutMaxCorrelation(sigArray1, sigArray2):
+    for k in range(nb_stations):
+        normalize(sigArray1[k])
+        normalize(sigArray2[k])
+    fig, axs = plt.subplots(nrows=nb_stations, ncols=2)
+
+    sig1k = sigArray1[0]
+    sig2k = sigArray2[0]
+
+    N = np.min((len(sig1k), len(sig2k)))
+    # On prend la taille minimale des signaux pour tronquer les autres signaux
+    # Car pour deux explosions différentes, les signaux ne seront pas de la même longueur.
+
+    lags = deltaT * \
+        sgn.correlation_lags(N, N)
+    # Calcul des retards dans les corrélations
+
+    L = len(lags)
+
+    # On initialise les tableaux d'intercorrélations et d'autocorrélations
+
+    maxCorrelations = np.empty(nb_stations)
+
+    for k in range(nb_stations):
+        sig1k = sigArray1[k, :N]
+        sig2k = sigArray2[k, :N]
+
+        interCorr = sgn.correlate(np.abs(sig1k[:, 1]), np.abs(sig2k[:, 1]))
+
+        axs[k, 0].plot(
+            lags, interCorr, label='correlation entre les signaux de la station ' + str(k))
+        axs[k, 0].legend()
+
+        axs[k, 1].plot(sig1k[:, 0], sig1k[:, 1],
+                       label='signal original station ' + str(k))
+        axs[k, 1].plot(sig2k[:, 0], sig2k[:, 1],
+                       label='signal autre explosion station ' + str(k))
+        axs[k, 1].legend()
+
+        maxCorrelations[k] = np.max(interCorr)
+    cost = 1 - np.mean(maxCorrelations)
+    plt.show()
+    print(cost)
+
+
+#coutMaxCorrelation(signalArray60, signalArray40)
+#coutMaxCorrelation(signalArray60, signalArray55)
+#coutMaxCorrelation(signalArray60, sigArray60Delayed)
+
+
+displayMatch(sigArray60Delayed[0], signalArray60[0])
+displayMatch(sigArray60Delayed[0], signalArray60[0], derivative=True)
